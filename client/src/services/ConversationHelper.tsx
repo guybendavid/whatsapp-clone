77153147b02f54f9ca0a869f6ccf2ed89a8616b8
Guy <guybdd123@gmail.com>
2020-12-14 17:05:22 +0200

    lastMessages of series

--- client/src/services/ConversationHelper.tsx
+++ client/src/services/ConversationHelper.tsx
@@ -1,18 +1,17 @@
 import { Message } from "../interfaces/interfaces";
 
 const messagesIdentifier = (messages: Message[], setFirstIndexesOfSeries: (indexes: any[]) => void) => {
-  const lastMessageOfSeries: Message[] = [];
+  const lastMessagesOfSeries: Message[] = [];
   // eslint-disable-next-line
   const indexes = messages.map((message: Message, index: number) => {
-
-    if (lastMessageOfSeries.length === 0) {
-      lastMessageOfSeries.push(message);
+    if (lastMessagesOfSeries.length === 0) {
+      lastMessagesOfSeries.push(message);
       return index;
     } else {
-      const lastMessage = lastMessageOfSeries[lastMessageOfSeries.length - 1];
+      const lastMessage = lastMessagesOfSeries[lastMessagesOfSeries.length - 1];
 
       if (message.senderId !== lastMessage.senderId) {
-        lastMessageOfSeries.push(message);
+        lastMessagesOfSeries.push(message);
         return index;
       }
     }
@@ -21,8 +20,7 @@ const messagesIdentifier = (messages: Message[], setFirstIndexesOfSeries: (index
   setFirstIndexesOfSeries(indexes);
 };
 
-// To do: fix the another fetch request when a new message is sent and maybe the delay will be removed also
-// Then try to move the merge logic of the conversation to apolloProvider
+// To do: fix the delay
 // may be attach onCompleted and onError to all apollo methods
 
 const classesGenerator = (senderId: string, loggedInUserId: string, firstIndexesOfSeries: any[], index: number) => {
