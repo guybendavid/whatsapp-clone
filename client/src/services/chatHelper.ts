f963229ffac869628ec21150d87f1c4db68d6e2f
Guy <guybdd123@gmail.com>
2021-06-15 20:56:44 +0300

    comment

--- client/src/services/chatHelper.ts
+++ client/src/services/chatHelper.ts
@@ -2,11 +2,12 @@ import { ApolloClient } from "@apollo/client";
 import { Message } from "interfaces/interfaces";
 import { GET_MESSAGES } from "./graphql";
 
+// To do: check why it is working without this also (apollo provider)
 const addNewMessageToChat = (newMessage: Message, client: ApolloClient<any>, selectedUserId: string) => {
   const queryToUpdate = { query: GET_MESSAGES, variables: { otherUserId: selectedUserId } };
 
   const newData = {
-    getMessages: [newMessage],
+    getMessages: [newMessage]
   };
 
   client.writeQuery({ ...queryToUpdate, data: { getMessages: newData } });
