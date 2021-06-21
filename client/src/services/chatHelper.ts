ae02e202f7eb249767b67e42d77fdea2d5945f53
Guy <guybdd123@gmail.com>
2021-06-21 21:35:11 +0300

    updating the messages array directly from writeQuery

--- client/src/services/chatHelper.ts
+++ client/src/services/chatHelper.ts
@@ -4,7 +4,9 @@ import { GET_MESSAGES } from "./graphql";
 
 const addNewMessageToChat = (newMessage: Message, client: ApolloClient<any>, selectedUserId: string) => {
   const queryToUpdate = { query: GET_MESSAGES, variables: { otherUserId: selectedUserId } };
-  client.writeQuery({ ...queryToUpdate, data: { getMessages: [newMessage] } });
+  const data = client.readQuery(queryToUpdate);
+  console.log(data.getMessages);
+  client.writeQuery({ ...queryToUpdate, data: { getMessages: [...data.getMessages, newMessage] } });
 };
 
 export { addNewMessageToChat };
