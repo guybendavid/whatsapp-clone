acc80a2b2ee5af2676e13df2fa1a8b0615a4951f
Guy <guybdd123@gmail.com>
2021-06-29 13:24:39 +0300

    removing unnecessary try catch blocks from resolvers

--- server/graphql/resolvers/messages.ts
+++ server/graphql/resolvers/messages.ts
@@ -1,4 +1,4 @@
-import { UserInputError, ApolloError, withFilter, PubSub } from "apollo-server";
+import { UserInputError, withFilter, PubSub } from "apollo-server";
 import { Op } from "sequelize";
 import { User, Message } from "../../db/models/modelsConfig";
 import { Message as MessageInterface, User as IUser } from "../../db/interfaces/interfaces";
@@ -7,28 +7,23 @@ const messagesResolver = {
   Query: {
     getMessages: async (_parent: any, args: { otherUserId: string; }, { user }: { user: IUser; }) => {
       const { otherUserId } = args;
+      const otherUser = await User.findOne({ where: { id: otherUserId } });
 
-      try {
-        const otherUser = await User.findOne({ where: { id: otherUserId } });
-
-        if (!otherUser) {
-          throw new UserInputError("User not found");
-        }
-
-        const ids = [user.id, otherUserId];
-
-        const messages = await Message.findAll({
-          where: {
-            senderId: { [Op.in]: ids },
-            recipientId: { [Op.in]: ids }
-          },
-          order: [["createdAt", "ASC"]]
-        });
-
-        return messages;
-      } catch (err) {
-        throw new ApolloError(err);
+      if (!otherUser) {
+        throw new UserInputError("User not found");
       }
+
+      const ids = [user.id, otherUserId];
+
+      const messages = await Message.findAll({
+        where: {
+          senderId: { [Op.in]: ids },
+          recipientId: { [Op.in]: ids }
+        },
+        order: [["createdAt", "ASC"]]
+      });
+
+      return messages;
     }
   },
   Mutation: {
@@ -39,13 +34,9 @@ const messagesResolver = {
         throw new UserInputError("You cant message yourself");
       }
 
-      try {
-        const message = await Message.create({ senderId: user.id, recipientId, content });
-        pubsub.publish("NEW_MESSAGE", { newMessage: message });
-        return message;
-      } catch (err) {
-        throw new ApolloError(err);
-      }
+      const message = await Message.create({ senderId: user.id, recipientId, content });
+      pubsub.publish("NEW_MESSAGE", { newMessage: message });
+      return message;
     }
   },
   Subscription: {
--- server/graphql/resolvers/users.ts
+++ server/graphql/resolvers/users.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import generateToken from "../../utils/generateToken";
-import { UserInputError, ApolloError } from "apollo-server";
+import { UserInputError } from "apollo-server";
 import { QueryTypes } from "sequelize";
 import { sequelize, User } from "../../db/models/modelsConfig";
 import { User as UserInterface } from "../../db/interfaces/interfaces";
@@ -14,80 +14,60 @@ const usersResolver = {
       const { id, offset, limit } = args;
       const getTotalUsers = "select count(id) from users";
       const getSidebarUsers = getUsersWithLatestMessage(offset, limit);
+      let totalUsers = await sequelize.query(getTotalUsers, { type: QueryTypes.SELECT });
 
-      try {
-        let totalUsers = await sequelize.query(getTotalUsers, { type: QueryTypes.SELECT });
+      if (totalUsers[0]?.count > 0) {
+        totalUsers = totalUsers[0].count - 1;
+        const sidebarUsers = await sequelize.query(getSidebarUsers, { type: QueryTypes.SELECT, replacements: [id, id, id, offset, limit] });
 
-        if (totalUsers[0]?.count > 0) {
-          totalUsers = totalUsers[0].count - 1;
-          const sidebarUsers = await sequelize.query(getSidebarUsers, { type: QueryTypes.SELECT, replacements: [id, id, id, offset, limit] });
+        sidebarUsers.forEach((user: any) => {
+          user.latestMessage = { content: user.content, createdAt: user.createdAt };
+          delete user.content;
+          delete user.createdAt;
+        });
 
-          sidebarUsers.forEach((user: any) => {
-            user.latestMessage = { content: user.content, createdAt: user.createdAt };
-            delete user.content;
-            delete user.createdAt;
-          });
-
-          return { users: sidebarUsers, totalUsersExceptLoggedUser: totalUsers };
-        } else {
-          return { users: [], totalUsersExceptLoggedUser: 0 };
-        }
-      } catch (err) {
-        throw new ApolloError(err);
+        return { users: sidebarUsers, totalUsersExceptLoggedUser: totalUsers };
+      } else {
+        return { users: [], totalUsersExceptLoggedUser: 0 };
       }
     },
     getUser: async (_parent: any, args: { id: string; }, _context: { user: UserInterface; }) => {
       const { id } = args;
-
-      try {
-        const user = await User.findOne({ where: { id } });
-        return user;
-      } catch (err) {
-        throw new ApolloError(err);
-      }
+      const user = await User.findOne({ where: { id } });
+      return user;
     }
   },
   Mutation: {
     register: async (_parent: any, args: UserInterface) => {
       const { firstName, lastName, username, password } = args;
+      const isUserExists = await User.findOne({ where: { username } });
 
-      try {
-        const isUserExists = await User.findOne({ where: { username } });
-
-        if (isUserExists) {
-          throw new UserInputError("Username already exists");
-        }
-
-        const hasedPassword = await bcrypt.hash(password as string, 6);
-        const image = generateImage();
-        const user = await User.create({ firstName, lastName, username, password: hasedPassword, image });
-        const { password: _userPassword, ...safeUserData } = user.toJSON();
-        return { ...safeUserData, token: generateToken({ id: user.id, firstName, lastName }) };
-      } catch (err) {
-        throw new ApolloError(err);
+      if (isUserExists) {
+        throw new UserInputError("Username already exists");
       }
+
+      const hasedPassword = await bcrypt.hash(password as string, 6);
+      const image = generateImage();
+      const user = await User.create({ firstName, lastName, username, password: hasedPassword, image });
+      const { password: _userPassword, ...safeUserData } = user.toJSON();
+      return { ...safeUserData, token: generateToken({ id: user.id, firstName, lastName }) };
     },
     login: async (_parent: any, args: UserInterface) => {
       const { username, password } = args;
+      const user = await User.findOne({ where: { username } });
 
-      try {
-        const user = await User.findOne({ where: { username } });
-
-        if (!user) {
-          throw new UserInputError("User not found");
-        }
-
-        const correctPassword = await bcrypt.compare(password as string, user.password);
-
-        if (!correctPassword) {
-          throw new UserInputError("Password is incorrect");
-        }
-
-        const { id, firstName, lastName, image } = user;
-        return { id, firstName, lastName, username, image, token: generateToken({ id, firstName, lastName }) };
-      } catch (err) {
-        throw new ApolloError(err);
+      if (!user) {
+        throw new UserInputError("User not found");
       }
+
+      const correctPassword = await bcrypt.compare(password as string, user.password);
+
+      if (!correctPassword) {
+        throw new UserInputError("Password is incorrect");
+      }
+
+      const { id, firstName, lastName, image } = user;
+      return { id, firstName, lastName, username, image, token: generateToken({ id, firstName, lastName }) };
     }
   }
 };
