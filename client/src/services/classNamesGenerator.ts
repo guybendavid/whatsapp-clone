b9d748cde8eac47121eda9ae31c70608c20074d8
Guy <guybdd123@gmail.com>
2021-09-20 18:45:46 +0300

    removing old services

--- client/src/services/classNamesGenerator.ts
+++
@@ -1,3 +0,0 @@
-type ArrayItem = string | boolean | undefined;
-const classNamesGenerator = (...items: ArrayItem[]) => [...items].filter(string => string).join(' ');
-export default classNamesGenerator;
