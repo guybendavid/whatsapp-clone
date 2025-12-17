89b5c8e3acea8716c14b7ebf6b9adc54a4d4f19d
Guy <bendavidguy1@gmail.com>
2025-12-17 21:44:19 +0200

    chore: update Vite configuration and add vite-tsconfig-paths for improved path resolution

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,22 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      ApolloProvider: path.resolve(__dirname, "./src/ApolloProvider"),
-      App: path.resolve(__dirname, "./src/App"),
-      AppRouter: path.resolve(__dirname, "./src/AppRouter"),
-      components: path.resolve(__dirname, "./src/components"),
-      contexts: path.resolve(__dirname, "./src/contexts"),
-      images: path.resolve(__dirname, "./src/images"),
-      services: path.resolve(__dirname, "./src/services"),
-      styles: path.resolve(__dirname, "./src/styles"),
-      types: path.resolve(__dirname, "./src/types")
-    }
-  },
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000,
     open: true
